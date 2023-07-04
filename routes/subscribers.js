9e9ecfcefd0b54ee64a3e614dcf2b4f968564d07
AhmadOlagunju <133206365+AhmadOlagunju@users.noreply.github.com>
2023-07-03 18:41:08 -0600

    test routes with rest client

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+//Getting all
+router.get('/', (req, res) => {
+    res.send("Hello World")
+})
+//Getting one
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+})
+//Posting/Creating one
+router.post('/', (req, res) => {
+    
+})
+//Patching/Updating one
+router.patch('/:id', (req, res) => {
+    
+})
+//Deleting one
+router.delete('/:id', (req, res) => {
+    
+})
 module.exports = router
